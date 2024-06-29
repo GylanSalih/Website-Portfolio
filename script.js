b9ed6477d643da1d79f81ff5a88ef4e9cb371af9
Gylan <gylan.salih@outlook.de>
2024-06-29 03:29:55 +0200

    update

--- script.js
+++ script.js
@@ -129,3 +129,28 @@ setTimeout(function() {
     });
   });
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#js-scroll"),
+    smooth: true,
+    smoothMobile: true,
+    inertia: 0.75,
+  });
